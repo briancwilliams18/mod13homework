3f95bb14d1cd86a35580056355ea6932200b74aa
Brian Williams <briancwilliams18@gmail.com>
2023-08-25 14:39:33 -0400

    Sync Sequelize to Database on Server Start:

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./models'); // Import sequelize connection
 
-router.use('/api', apiRoutes);
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => { // Use sync() method to sync models
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
-
-module.exports = router;
